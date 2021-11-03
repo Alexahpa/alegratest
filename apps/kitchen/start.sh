f3222668401d6510f18a61ef1d4b87e6d9cbd70b
Alexandra Huamani <a.huamani@pucp.pe>
2021-11-03 01:32:48 -0500

    Last fixes

--- apps/kitchen/start.sh
+++ apps/kitchen/start.sh
@@ -2,12 +2,16 @@ echo "start profiling"
 TPS=$(date +%s)
 echo "${TPS}"
 
+cp .env.example .env
 php artisan key:generate
 
 touch /var/www/kitchen/storage/logs/laravel.log
 
-php artisan queue:work --queue=kitchen  --tries=3 &
+echo "Waiting for db"
+sleep 45
+
 php artisan migrate --force
+php artisan queue:work --queue=kitchen  --tries=3 &
 
 echo "Is running"
 php-fpm
--- apps/storage/start.sh
+++ apps/storage/start.sh
@@ -1,14 +1,17 @@
 echo "start profiling"
 TPS=$(date +%s)
 echo "${TPS}"
+cp .env.example .env
 
 php artisan key:generate
 
 touch /var/www/kitchen/storage/logs/laravel.log
 
+echo "Waiting for db"
+sleep 55
 
-php artisan queue:work  --queue=default,storage  --tries=3 &
 php artisan migrate --force
+php artisan queue:work  --queue=default,storage  --tries=3 &
 
 echo "Is running"
 php-fpm
